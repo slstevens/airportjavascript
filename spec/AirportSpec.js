76029c524515ccb4aa32a2f68d9a1e363c94d87b
slstevens <sean.stevens.123@gmail.com>
2014-10-16 17:26:56 +0100

    Update

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,23 +1,88 @@
 describe('Airport', function() {
 
-	beforeEach(function() {
-		plane = new Plane;
-		airport = new Airport;
-	})
+		beforeEach(function() {
+			plane = new Plane;
+			airport = new Airport;
+		})
 
-	it('can have planes', function() {
-		expect(airport.planes).toEqual([]);
+
+	describe('with planes', function() {
+		
+		fillUp = function(airport) {
+			for(var i = 0; i < 40; i++) {
+				airport.land(new Plane)
+			}
+		};
+
+		it('can have planes', function() {
+			expect(airport.planes).toEqual([]);
+		});
+
+		it('can allow a plane to land', function() {
+			airport.land(plane);
+			expect(airport.planes).toEqual([plane]);
+		});
+
+		it('can allow a plane to takeoff', function(){
+			airport.land(plane);
+			airport.takeoff(plane);
+			expect(airport.planes).toEqual([]);
+		});
+
+		it('should only allow a plane to takeoff that is in the airport', function() {
+			boeing = new Plane;
+			airport.land(boeing);
+			airport.land(plane);
+			airport.takeoff(boeing);
+			expect(airport.planes).toEqual([plane])
+		});
+
+		it('should ensure that a plane has landed before taking off', function() {
+			boeing = new Plane;
+			airport.land(plane)
+			airport.takeoff(boeing)
+			expect(airport.planes.length).toEqual(1)
+		});
+
+		it('should know if its full', function() {
+			fillUp(airport)
+			expect(airport.isFull()).toBe(true)
+		});
+
+		it('should know if its not full', function() {
+			expect(airport.isFull()).toBe(false)
+		});
+
+		xit('should know how many planes are in the airport', function() {
+			airport.land(plane)
+			expect(airport.planes.length).toEqual(1)
+		});
+
+		xit('should not exceed its capacity', function() {
+			fillUp(airport)
+			airport.land(new Plane)
+			expect(airport.planes.length).toEqual(40)
+		});
 	});
 
-	it('can allow a plane to land', function() {
-		airport.land(plane);
-		expect(airport.planes).toEqual([plane]);
-	});
+	describe('with people', function() {
 
-	it('can allow a plane to takeoff', function(){
-		airport.land(plane);
-		airport.takeoff(plane);
-		expect(airport.planes).toEqual([]);
+		it('has passengers', function() {
+			expect(airport.passengers).toEqual([])
+		});
+
+		xit('has a maximum capacity of passengers', function() {
+			expect(airport.passengers.length).toEqual(1000)
+		});
+
+		xit('it can accept passengers into it', function() {
+			exp
+		});
+
+		xit('can transfer(release) passengers from airport', function() {
+
+		})
+
 	});
 });
 
--- src/Airport.js
+++ src/Airport.js
@@ -1,17 +1,43 @@
 function Airport() {
 	this.planes = []
+	this.passengers = []
+	// this.passengers.length = 1000
 };
 
 function Plane() {
-
 }
 
 Airport.prototype.land = function(plane) {
-	this.planes.push(plane)
-	return plane;
+	// if (!this.isFull())
+		this.planes.push(plane)
+	// return plane
 };
 
 Airport.prototype.takeoff = function(plane) {
-	this.planes.pop(plane)
-	return plane
+	if(this._isLanded(plane))
+		this._clearToTakeoff(plane);
 };
+
+Airport.prototype.isFull = function() {
+	return this.planes.length === 40
+};
+
+
+
+Airport.prototype._clearToTakeoff = function(plane) {
+	return this._clearManyPlanesToTakeOff(plane, 1)[0];
+};
+
+Airport.prototype._clearManyPlanesToTakeOff = function(plane, numberOfPlanes) {
+	return this.planes.splice(this._findLocationOf(plane), numberOfPlanes);
+};
+
+Airport.prototype._isLanded = function(plane) {
+	var notFound = -1
+	return this._findLocationOf(plane) !== notFound;
+};
+
+Airport.prototype._findLocationOf = function(plane) {
+	return this.planes.indexOf(plane) 
+};
+
